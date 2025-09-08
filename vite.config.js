43980026714b9aef16bf64158a487976b2c1448d
Joe Walker <joewalker@Joes-MacBook-Pro.local>
2025-09-08 12:40:15 -0700

    updating repo paths and such

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// vite.config.js
-export default {
-  base: '/lab-inventory-app/',
-}
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === 'production' ? '/lab-inventory-app/' : '/',
+}))
